48664d53e212a7feb679ba6c60907bd8179ea648
boomb0om <igorekandco1@gmail.com>
2023-08-23 17:08:59 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import pkg_resources
+from setuptools import setup, find_packages
+
+
+setup(
+    name="CRAFT",
+    py_modules=["CRAFT"],
+    version="1.0",
+    description="",
+    author="Igor Pavlov, Youngmin Baek, Bado Lee, Dongyoon Han, Sangdoo Yun, Hwalsuk Lee",
+    url='https://github.com/boomb0om/CRAFT-text-detection',
+    packages=find_packages(include=['CRAFT']),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
+)
